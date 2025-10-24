07c66dfc5cc9cca37e24dd31e3af05809ab897a9
Caroline <caroline.markov@gmail.com>
2025-10-24 19:48:47 +0300

    KAN-41: Implement Hinting System
    Adds tiered hints (Hard, Medium, Easy) and penalizes the score by -1 when a hint is used.

--- app/src/main/java/com/example/culturequest/ui/theme/Type.kt
+++ app/src/main/java/com/example/culturequest/ui/theme/Type.kt
@@ -10,23 +10,23 @@ import androidx.compose.ui.unit.sp
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Medium,
         fontSize = 30.sp,
-        lineHeight = 24.sp,
+        lineHeight = 36.sp,
         letterSpacing = 0.5.sp
     ),
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Bold,
         fontSize = 40.sp,
         lineHeight = 50.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
         letterSpacing = 0.5.sp
     )
 
